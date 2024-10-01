fbdd77f0a91ff0af89f80cc091e6d87fc2b6869b
菅沼 洋平 <yohei@suganumanoMacBook-Air.local>
2024-10-01 11:06:51 +0900

    7

--- src/app.py
+++ src/app.py
@@ -1,63 +1,79 @@
+# 必要なモジュールのインポート
 import joblib
 from flask import Flask, request, render_template
 from wtforms import Form, FloatField, SubmitField, validators
 import numpy as np
 
+# 学習済みモデルをもとに推論する関数
 def predict(x):
+    # 学習済みモデル（iris.pkl）を読み込み
     model = joblib.load('./src/iris.pkl')
-    x = x.reshape(1, -1)
+    x = x.reshape(1,-1)
     pred_label = model.predict(x)
     return pred_label
 
+# 推論したラベルから花の名前を返す関数
 def getName(label):
     if label == 0:
-        return "Setosa"
+        return 'Iris Setosa'
     elif label == 1:
-        return "Versicolor"
+        return 'Iris Versicolor'
     elif label == 2:
-        return "Virginica"
+        return 'Iris Virginica'
     else:
-        return "Error"
+        return 'Error'
 
+# Flask のインスタンスを作成
 app = Flask(__name__)
 
+# 入力フォームの設定
 class IrisForm(Form):
-    SepalLength = FloatField('がくの長さ(0cm 〜 10cm)',
-                        [validators.InputRequired(),
-                         validators.NumberRange(min=0, max=10, message='0〜10の数値を入力してください')])
-    
-    SepalWidth = FloatField('がくの幅(0cm 〜 5cm)',
-                        [validators.InputRequired(),
-                         validators.NumberRange(min=0, max=5, message='0〜5の数値を入力してください')])
+    SepalLength = FloatField('がくの長さ (0cm ~ 10cm)',
+                    [validators.InputRequired(),
+                    validators.NumberRange(min=0, max=10, message='0〜10の数値を入力してください')])
 
-    PetalLength = FloatField('花弁の長さ(0cm 〜 10cm)',
-                        [validators.InputRequired(),
-                         validators.NumberRange(min=0, max=10, message='0〜10の数値を入力してください')])
-    
-    PetalWidth = FloatField('花弁の幅(0cm 〜 5cm)',
-                        [validators.InputRequired(),
-                         validators.NumberRange(min=0, max=10, message='0〜5の数値を入力してください')])
+    SepalWidth  = FloatField('がくの幅 (0cm ~ 5cm)',
+                    [validators.InputRequired(),
+                    validators.NumberRange(min=0, max=5, message='0〜5の数値を入力してください')])
 
+    PetalLength = FloatField('花弁の長さ (0cm ~ 10cm)',
+                    [validators.InputRequired(),
+                    validators.NumberRange(min=0, max=10, message='0〜10の数値を入力してください')])
+
+    PetalWidth  = FloatField('花弁の幅 (0cm ~ 5cm)',
+                    [validators.InputRequired(),
+                    validators.NumberRange(min=0, max=5, message='0〜5の数値を入力してください')])
+
+    # HTML で表示する submit ボタンの設定
     submit = SubmitField('判定')
 
+# URL にアクセスがあった場合の挙動の設定
 @app.route('/', methods = ['GET', 'POST'])
 def predicts():
+    # フォームの設定 IrsiForm 　クラスをインスタンス化
     irisForm = IrisForm(request.form)
-
+    # POST メソッドの定義
     if request.method == 'POST':
+
+        # 条件に当てはまる場合
         if irisForm.validate() == False:
             return render_template('index.html', forms=irisForm)
+        # 条件に当てはまらない場合、推論を実行
         else:
             VarSepalLength = float(request.form['SepalLength'])
-            VarSepalWidth = float(request.form['SepalWidth'])
+            VarSepalWidth  = float(request.form['SepalWidth'])
             VarPetalLength = float(request.form['PetalLength'])
-            VarPetalWidth = float(request.form['PetalWidth'])
+            VarPetalWidth  = float(request.form['PetalWidth'])
+            # 入力された値を ndarray に変換して推論
             x = np.array([VarSepalLength, VarSepalWidth, VarPetalLength, VarPetalWidth])
             pred = predict(x)
             irisName_ = getName(pred)
             return render_template('result.html', irisName=irisName_)
+
+    # GET メソッドの定義
     elif request.method == 'GET':
         return render_template('index.html', forms=irisForm)
-    
+
+# アプリケーションの実行
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run()
--- src/iris.py
+++ src/iris.py
@@ -1,21 +1,28 @@
+# 必要なモジュールのインポート
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn import svm
 from sklearn.metrics import classification_report
 import joblib
 
+# データ取得
 iris = load_iris()
 
-x = iris.data
-t = iris.target
+#　入力変数と出力変数に切り分け
+x, t = iris.data, iris.target
 
+# 学習用データとテスト用データに分割
 x_train, x_test, t_train, t_test = train_test_split(x, t, test_size=0.3, random_state=0)
 
+# サポートベクターマシンのインスタンスを作成
 model = svm.LinearSVC()
 
+# 学習と推論
 model.fit(x_train, t_train)
 pred = model.predict(x_test)
 
+# 予測精度を確認
 print(classification_report(t_test, pred))
 
-joblib.dump(model, "src/iris.pkl", compress=True)
+# 学習済みモデルを保存
+joblib.dump(model, 'src/iris.pkl', compress=True)
